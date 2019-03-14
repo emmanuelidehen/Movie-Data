9b983d3abee910aaee8712bb6d8f141fc052fe48
Emmanuel idehen <emmanuel.idehen@mvsu.edu>
2019-03-14 11:25:09 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,53 @@
 //
 //  main.cpp
-//  Movie
+//  Example
 //
-//  Created by Emmanuel Idehen on 2/5/19.
+//  Movie Data !
+//  Created by Emmanuel Idehen on 2/7/19.
 //  Copyright © 2019 Emmanuel Idehen. All rights reserved.
 //
 
 #include <iostream>
 #include <string>
-
 using namespace std;
-#include "Movie.h"
 
-int main() {
+
+struct MovieData
+{
     
-   
-    Movie person;
-    person.year = 2012;
-    person.director = "mr paul";
-    person.runtime = 12.0;
-    person.title = "the god's of egpyt";
-  //  Movie::Movie(Movie& person, int year, double runtime, std::string director, std::string title);
-   // Movie(Movie& person, 2012, 12.0, "mr paul", "the god's of egpyt");
-   
-  void display(Movie& person);
- 
+    string director;
+    int runtime;
+    string title;
+    int year;
     
     
+    MovieData(string directors, int runtimes, string titles, int years)
+    {
+        director = directors;
+        runtime = runtimes;
+        title = titles;
+        year = years;
+        
+    }
+    
+};
+
+
+void print (MovieData x)
+{
+    cout << x.director << "  " << x.runtime <<"  " << x.title <<"   "<< x.year << endl;
     
     
+}
+
+int main()
+{
     
+    MovieData obj1("hey",0,"hh",0);
+    MovieData obj2("hey",0,"kk",0);
     
+    print(obj1);
+    print(obj2);
     
     return 0;
 }
